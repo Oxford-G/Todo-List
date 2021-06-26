e80bda2f744b8e493cc3d7a9d2c748c6d77abe9d
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-26 12:54:36 +0100

    add const predefinedProjects

--- src/start.js
+++ src/start.js
@@ -7,3 +7,19 @@ import {
 } from './task';
 
 const container = document.getElementById('content');
+
+const predefinedProjects = [new Project('Default'), new Project('Yesterday'), new Project('Today'), new Project('Tomorrow')];
+function getProjects() {
+  if (localStorage.getItem('toDoProjects')) {
+    const projects = JSON.parse(localStorage.getItem('toDoProjects'));
+    for (let i = 0; i < projects.length; i += 1) {
+      Object.setPrototypeOf(projects[i], Project.prototype);
+      const todoList = projects[i].todos;
+      for (let i = 0; i < todoList.length; i += 1) {
+        Object.setPrototypeOf(todoList[i], Todo.prototype);
+      }
+    }
+    return projects;
+  }
+  return predefinedProjects;
+}
