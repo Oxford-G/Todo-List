a0c08a6f6ed66d74fed033f4ea4294cf248fe386
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-26 12:51:28 +0100

    add const projectModule

--- src/project.js
+++ src/project.js
@@ -8,3 +8,22 @@ class Project {
     this.todos = [];
   }
 }
+
+const projectModule = (() => {
+  function createProject(target) {
+    const projects = getProjects();
+    const targetValue = target.value.toLowerCase();
+    const invalidName = projects.find((element) => element.name.toLowerCase() === targetValue);
+
+    if (target.value !== '' && !invalidName) {
+      const newProject = new Project(target.value);
+      projects.push(newProject);
+      localStorage.toDoProjects = JSON.stringify(projects);
+
+      start();
+    }
+    target.value = '';
+  }
+
+  return { createProject };
+})();
