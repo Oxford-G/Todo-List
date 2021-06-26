4b6f2b4fd0566e3d30437b81975eda4f754d6515
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-26 10:26:29 +0100

    add dist folder

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("Hello webpack!")
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
