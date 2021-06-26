bbad87440ad83b052263a37aa0cbb5c1dca65ef3
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-06-26 13:04:26 +0100

    add function displayFooter

--- src/structure.js
+++ src/structure.js
@@ -8,3 +8,14 @@ function displayNav() {
   navBrand.setAttribute('class', 'navbar-brand');
   navBrand.textContent = 'To Do List';
 }
+
+function displayFooter() {
+  const container = document.getElementById('content');
+  const footer = container.appendChild(document.createElement('footer'));
+  footer.setAttribute('class', 'bg-primary');
+  const authors = footer.appendChild(document.createElement('p'));
+  authors.setAttribute('class', 'text-center text-light my-auto');
+  authors.innerHTML = 'Copyright 2021  Kene && John';
+}
+
+export { displayNav, displayFooter };
